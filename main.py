47355a1a2b43db26a859ad7cc950793e852b7681
Leonardo Borck <l.borck@edu.univali.br>
2021-10-15 14:32:36 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,20 +1,23 @@
 import cv2
 
-imagemOriginal = cv2.imread("dataset/1B1B2K1-1B6-5N2-6k1-8-8-8-4nq2.jpeg")
+imagemOriginal = cv2.imread("dataset/1b1b2K1-1r6-2P4p-1b1p4-4N1k1-3B2Q1-2q5-2N2q2.jpeg")
 imagemCinza = cv2.cvtColor(imagemOriginal, cv2.COLOR_BGR2GRAY)
 ret,imagemThreshold = cv2.threshold(imagemCinza,90,255,cv2.THRESH_BINARY_INV)
+imgCanny = cv2.Canny(imagemThreshold, 0, 50, apertureSize=5)
 area = 10
 cont = 0
-for i in range(25,imagemThreshold.shape[0],50):
-    for j in range(15,imagemThreshold.shape[1],50):
+for i in range(25,imgCanny.shape[0],50):
+    for j in range(15,imgCanny.shape[1],50):
         for aux in range(0,20):            
-            pixel = imagemThreshold[i,j+aux]
+            pixel = imgCanny[i,j+aux]
             if(pixel == 255):
                 cont+=pixel
-        media = cont/(area*2)
+        for aux in range(0,20):
+            pixel = imgCanny[i+(aux-area),j]
+        media = cont/(area*4)
         cont = 0
-        if(media>10):
-            print("peca")
+        print(media)
+                
 cv2.imshow("Imagem Original",imagemOriginal)
-cv2.imshow("Limiarizada",imagemThreshold)
+cv2.imshow("Limiarizada",imgCanny)
 
